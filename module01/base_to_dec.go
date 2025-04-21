91189c7d55f623713f55ac835998b91ca0a8d263
chrisinoma <chrisinoma@gmail.com>
2025-04-21 04:31:20 +0000

    fifth and sixth algorithms

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,13 +1,26 @@
 package module01
 
+import (
+	"math"
+	"strings"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
 // Eg:
 //
-//   BaseToDec("E", 16) => 14
-//   BaseToDec("1110", 2) => 14
-//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
 func BaseToDec(value string, base int) int {
-	return 0
+	var res float64
+	var pow int
+	const charset = "0123456789ABCDEF"
+	for i := len(value) - 1; i >= 0; i-- {
+		v := value[i]
+		b := strings.Index(charset, string(v))
+		res += float64(b) * math.Pow(float64(base), float64(pow))
+		pow++
+	}
+	return int(res)
 }
--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,14 +1,33 @@
 package module01
 
+import (
+	"fmt"
+)
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
 //
 // Eg:
 //
-//   DecToBase(14, 16) => "E"
-//   DecToBase(14, 2) => "1110"
-//
+//	DecToBase(14, 16) => "E"
+//	DecToBase(14, 2) => "1110"
 func DecToBase(dec, base int) string {
-	return ""
+	var res string
+	const charset = "0123456789ABCDEF"
+	for dec > 0 {
+		rem := dec % base
+		res = fmt.Sprintf("%v%s", string(charset[rem]), res)
+		dec = dec / base
+	}
+	return res
+
+	// ALt cleaner solution
+	// var res string
+	// for dec > 0 {
+	// 	rem := dec % base
+	// 	res = fmt.Sprintf("%X%s", rem, res)
+	// 	dec = dec / base
+	// }
+	// return res
 }
