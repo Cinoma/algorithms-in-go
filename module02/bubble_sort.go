bb3a6984158f43fac5e43a42e6abcff2c9512041
chrisinoma <chrisinoma@gmail.com>
2025-04-24 05:17:04 +0000

    bubble sort initial implementations

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -1,16 +1,30 @@
 package module02
 
-import "sort"
+import (
+	"sort"
+)
 
 // BubbleSortInt will sort a list of integers using the bubble sort algorithm.
 //
 // Big O: O(N^2), where N is the size of the list
 func BubbleSortInt(list []int) {
+	for i := range len(list) - 1 {
+		if list[i] > list[i+1] {
+			list[i], list[i+1] = list[i+1], list[i]
+			BubbleSortInt(list)
+		}
+	}
 }
 
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
 // practice.
 func BubbleSortString(list []string) {
+	for i := range len(list) - 1 {
+		if list[i] > list[i+1] {
+			list[i], list[i+1] = list[i+1], list[i]
+			BubbleSortString(list)
+		}
+	}
 }
 
 // BubbleSortPerson uses bubble sort to sort Person slices by: Age, then
